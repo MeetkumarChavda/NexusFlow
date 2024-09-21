c804791a523bd0c069b422367ba290e01d054e8d
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-09-21 14:48:08 +0530

    feat:Properties detail page added

---
+++ nexusflow-front/app/components/properties/ReservationSidebar.tsx
@@ -0,0 +1,35 @@
+const ReservationSidebar = () =>{
+    return(
+       <aside className="p-6 mt-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
+            <h2 className="mb-5 text-2xl">$200 per night</h2>
+            
+            <div className="mb-6 p-3 border border-gray-400 rounded-xl">
+                <label className="block mb-2 text-xs font-bold">Guest</label>
+                <select className="w-full -ml-1 text-xm">
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                    <option value="">4</option>
+                </select>
+            </div>
+
+            <div className="w-full mb-6 py-6 text-center text-white bg-nexus hover:bg-nexus-dark rounded-xl">Book</div>
+            <div className="mb-4 flex justify-between items-center">
+                <p>$200 * 4 nights</p>
+                <p>$800</p>
+            </div>
+
+            <div className="mb-4 flex justify-between items-center">
+                <p>nexus fee</p>
+                <p>$40</p>
+            </div>
+            <hr />
+
+            <div className="mt-4 flex justify-between items-center font-bold">
+                <p>Total</p>
+                <p>$840</p>
+            </div>
+       </aside>
+    )
+}
+export default ReservationSidebar
