a964c0d612721d64f836d49390f8dd53cff8b1cd
Darshil0109 <122811740+Darshil0109@users.noreply.github.com>
2024-09-24 17:43:34 +0530

    feat: Reservations Added

--- nexusflow-front/app/components/navbar/UserNav.tsx
+++ nexusflow-front/app/components/navbar/UserNav.tsx
@@ -1,18 +1,97 @@
+<<<<<<< Updated upstream
 const UserNav = () => {
     return(
        <div className="p-2 relative inline-block border rounded-full">
         <button className="flex items-center">
+=======
+'use client'
 
-            <svg  fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import MenuLink from "./MenuLink";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useSignupModal from "@/app/hooks/useSignupModal";
+import LogoutButton from '../LogoutButton';
 
-            <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-            </svg>
+interface UserNavProps {
+    userId?: string | null;
+}
 
+const UserNav: React.FC<UserNavProps> = ({
+    userId
+}
+) => {
+    const router = useRouter();
+    const [isOpen, setIsOpen] = useState(false)
+    const signupModal = useSignupModal();
+    const loginModal = useLoginModal();
+    return (
+        <div className="p-2 relative inline-block border rounded-full">
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center">
+>>>>>>> Stashed changes
+
+                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+
+                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+
+<<<<<<< Updated upstream
         </button>
        </div>
+=======
+            </button>
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    {userId ?
+                        (<>
+                            <MenuLink
+                                label='My properties'
+                                onClick={() => {
+                                    setIsOpen(false);
+                                    router.push('/myproperties');
+                                }}
+                            />
+                             <MenuLink
+                                label='My reservations'
+                                onClick={() => {
+                                    setIsOpen(false);
+                                    router.push('/myreservations');
+                                }}
+                            />
+                            <LogoutButton />
+                        </>
+                        ) : (
+                            <>
+                                <MenuLink
+                                    label='Log in'
+                                    onClick={() => {
+                                        setIsOpen(false);
+                                        loginModal.open()
+                                    }}
+                                />
+                                <MenuLink
+                                    label='Sign up'
+                                    onClick={() => {
+                                        setIsOpen(false);
+                                        signupModal.open()
+                                    }}
+                                />
+                            </>
+                        )
+                    }
+
+
+
+                </div>
+            )
+            }
+        </div>
+>>>>>>> Stashed changes
     )
 }
 
--- nexusflow-front/app/components/properties/PropertyListItem.tsx
+++ nexusflow-front/app/components/properties/PropertyListItem.tsx
@@ -1,6 +1,24 @@
+<<<<<<< Updated upstream
 import Image
  from "next/image";
 const PropertyListItem = () =>{
+=======
+import Image from "next/image";
+import { PropertyType } from "./PropertyList";
+import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
+
+
+interface PropertyProps{
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
+}
+const PropertyListItem:React.FC<PropertyProps> = ({
+    property,
+    markFavorite
+}) =>{
+    const router = useRouter();
+>>>>>>> Stashed changes
 
     return(
        <div className="cursor-pointer">
@@ -12,6 +30,13 @@ const PropertyListItem = () =>{
                 alt = 'property image'
                 className="hover:scale-110 object-cover transform h-full w-full"
             />
+             {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        is_favorite={property.is_favorite}
+                        markFavorite={(is_favorite) => markFavorite(is_favorite)}
+                    />
+                )}
         </div>
         <div className="mt-2">
             <div className="text-lg font-bold">Property Name</div>
