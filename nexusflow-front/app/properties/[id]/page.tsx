a8b9372f61eb318646a6646a6dfd27d9511944b8
Bhavya Patel <bhavyabpatel782@gmail.com>
2024-09-24 13:38:02 +0300

    land lord page dynamic completed

--- nexusflow-front/app/properties/[id]/page.tsx
+++ nexusflow-front/app/properties/[id]/page.tsx
@@ -1,11 +1,16 @@
+
+
 import Image from "next/image";
+import Link from "next/link";
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
 
 const PropertyDetailPage = async (
     {params}: { params: {id: string }}) => {
    
     const property = await apiService.get(`/api/properties/${params.id}`)
+    const userId = await getUserId();
 
     return(
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
@@ -23,8 +28,11 @@ const PropertyDetailPage = async (
                     <span className="mb-6 block text-lg text-gray-600">
                     {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms</span>
                     <hr />
-                    <div className="py-6 flex items-center space-x-4">
-                    {property.landlord.avatar_url && (
+                    <Link 
+                        href={`/landlords/${property.landlord.id}`}
+                        className="py-6 flex items-center space-x-4"
+                    >
+                        {property.landlord.avatar_url && (
                             <Image
                                 src={property.landlord.avatar_url}
                                 width={50}
@@ -33,8 +41,9 @@ const PropertyDetailPage = async (
                                 alt="The user name"
                             />
                         )}
+
                         <p><strong>{property.landlord.name}</strong> is your host</p>
-                    </div>
+                    </Link>
                     <hr />
                     <p className="mt-6 text-lg">
                         {property.description}
@@ -42,6 +51,7 @@ const PropertyDetailPage = async (
                 </div>
                 <ReservationSidebar 
                     property={property}
+                    userId={userId}
                 />
             </div>
         </main>
